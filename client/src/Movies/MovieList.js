23eb866890cc41c9c3ee5029affb0cebcdf08147
van jordan <vanjordan46@hotmail.com>
2019-05-20 15:04:46 -0700

    added missing files

---
+++ client/src/Movies/MovieList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="movie-list">
+        {this.state.movies.map(movie => (
+         <MovieDetails key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
+}
+
+function MovieDetails({ movie }) {
+  
+  return (
+
+     <MovieCard movie={movie} />
+  );
+}
---
+++ client/src/Movies/SavedList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class SavedList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="saved-list">
+        <h3>Saved Movies:</h3>
+        {this.props.list.map(movie => (
+          <Link to={`/movies/${movie.id}`} > <span className="saved-movie">{movie.title}</span></Link>
+        ))}
+        <Link to="/"><div className="home-button">Home</div></Link>
+      </div>
+    );
+  }
+}
