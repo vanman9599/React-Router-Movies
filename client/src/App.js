99aac8297a9f3cab76c7ef85092b89550fae959d
van jordan <vanjordan46@hotmail.com>
2019-05-20 14:40:51 -0700

    Deleted previous repository to clone again, and re-create project

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -22,7 +23,11 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={ (props)=>{
+            return (<Movie {...props} addToSavedList={this.addToSavedList} />
+            )}} />
+        
       </div>
     );
   }
